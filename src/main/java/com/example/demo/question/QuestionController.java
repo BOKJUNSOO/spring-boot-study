53693a9cee742f7cc783c610d93f31cf6c645ee8
복준수 <brianbok97@gmail.com>
2025-04-08 14:53:34 +0900

    Feat: 질문 목록 작성을 위한 컨트롤러
    질문 목록을 만들기 위해 컨트롤러를 이용해 URL을 맵핑.
    모델 객체와 타임리프의 조합을 통해 HTML에서 Java 객체를 참조할 수 있도록 했다.
    ResponseBody 어노테이션을 사용하면 리턴값을 그대로 출력에서 타임리프로 템플릿 HTML 참조(타임리프의 디폴트값)

---
+++ src/main/java/com/example/demo/question/QuestionController.java
@@ -0,0 +1,38 @@
+package com.example.demo.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+
+
+// question/list URL 매핑을 하기 위한 컨트롤러
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+    // RequiredArgsConstructor 는 롬복이 제공하는 애너테이션
+    // final 이 붙은 속성을 포함하는 생성자를 자동으로 만들어준다.
+    // 따라서 QuestionController 가 생성할때 자동으로 QuestionRepository 객체가 주입
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+    public String list(Model model) {
+
+        // repository 를 이용하여 데이터를 가져온다.
+        List<Question> questionList = this.questionRepository.findAll();
+
+        // Model 객체에 "questionList" 라는 이름으로 questionList 객체를 저장
+        // 이는 자바 Class 와 template 에 html 과 연결고리 역할을 한다.
+        // model 객체를 참조하여 템플릿에서 그 값을 사용할 수 있다.
+        // `ViewResolver`이 그 역할을 수행한다
+        model.addAttribute("questionList",questionList);
+
+        // Thymeleaf 템플릿 엔진은
+        // 컨트롤러의 리턴값을 파싱하여 해당하는 html 파일을 리턴한다
+        return "question_list";
+    }
+}
